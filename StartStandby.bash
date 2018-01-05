f9857535f4296e10c6e93cff1e948f279b1d31d3
Raman Kathpalia <33638245+Raman-Kathpalia@users.noreply.github.com>
2018-01-04 20:57:49 -0500

    Update StartStandby.bash

--- StartStandby.bash
+++ StartStandby.bash
@@ -36,7 +36,7 @@ if [[ -z $(grep "mqm" /etc/group | grep "$LOGNAME") ]]; then
   ノ)    Lﾉ
 (_／
 	
-    Else you continue to see a bashing dancing cat!!!                                                 
+    Else you continue to see a bashing cat in a weird pose!                                                 
         "
            exit 10
 
@@ -45,46 +45,58 @@ if [[ -z $(grep "mqm" /etc/group | grep "$LOGNAME") ]]; then
 
 Set_MQ_Variable() {					# Box all variables in this module
 
-Polling_Interval_Value=''
+POLLING_INTERVAL=''
 TIMESTAMP=''
 #
-Polling_Interval_Value=20
+POLLING_INTERVAL=20
 TIMESTAMP=`date "+%Y%h%d_%H_%M_%S"`
-Activity_Logs=$HOME/Failed_Over_MQ_Instance
-export FILE_Lock=${Activity_Logs}/FILE_Lock.txt
-ACTIVITY_FILE=${Activity_Logs}/Activity_trail.txt
+FAILOVER_ACTIVITY_DIR=$HOME/MI
+export LOCK_FILE=${FAILOVER_ACTIVITY_DIR}/LOCK_FILE.txt
+ACTIVITY_FILE=${FAILOVER_ACTIVITY_DIR}/Activity_trail.txt
 
-MQ_INSTANCE_ELSEW=$(dspmq -o all | \
+DEFUNCT_QMgr=$(dspmq -o all | \
         sed '/(Running elsewhere)/!d' | \
         awk -F '[()]' '{print $2}')
 
 }
 
-Set_MQ_Variable				# Instantiate Variables
+Set_MQ_Variable						# Instantiate Variables
 
-mkdir -p ${Activity_Logs}		# Create dir
+mkdir -p ${FAILOVER_ACTIVITY_DIR}		
 
 Start_Standby() {
 
 Set_MQ_Variable						# Source MQ Variable defined above
 
-if [[ -n ${MQ_INSTANCE_ELSEW[@]} ]]; then		# If failed over Queue-Manager are found with status - (Running elsewhere), start them in standby mode
+if [[ -n ${DEFUNCT_QMgr[@]} ]]; then			# If failed over Queue-Manager are found with status - (Running elsewhere), start them in standby mode
 
-  for QMgr in ${MQ_INSTANCE_ELSEW[@]}; do
+  for QMgr in ${DEFUNCT_QMgr[@]}; do
 	
     echo " 
 
 *********************************************************************
 $TIMESTAMP : Initiating $QMgr in Standby.
 *********************************************************************
-     " >> $ACTIVITY_FILE				#CAPTURE ACTIVITY PERFORMED; WITH TIMESTAMP
+     " >> $ACTIVITY_FILE					#CAPTURE ACTIVITY PERFORMED; WITH TIMESTAMP
 
 	strmqm -x $QMgr	>> $ACTIVITY_FILE 2>&1		
+
+
+	if [[ $(sed -n '$=' $ACTIVITY_FILE) -gt 3000 ]];then	# Keep a check on Activity log. Roll off very old entries
+
+        sed -i '1,100d' $ACTIVITY_FILE
+	
+	fi
+
+
   done
 
 fi
 
+
+
 }
 
-	while [ -e $FILE_Lock ]; 
-		do Start_Standby; sleep $Polling_Interval_Value; done &	            # Keep polling
+	while [ -e $LOCK_FILE ]; 
+
+		do Start_Standby; sleep $POLLING_INTERVAL; done &	            # Keep polling
